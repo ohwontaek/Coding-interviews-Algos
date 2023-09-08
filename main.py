5f99ce9c3f35c40d7e2201f6f7a115fdf731e77a
‘ohwontaek’ <brunofied@gmail.com>
2023-09-08 11:26:50 +0900

    just

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import sys
-from typing import List
-
-
-def maxProfit(self, prices: List[int]) -> int:
-   profit = 0
-   min_price = sys.maxsize
-   #최소값과 최대값을 계속 갱신
-   for price in prices:
-       min_price = min(min_price, price)
-       profit = max(profit, price - min_price)
-
-       return profit
+def solution(park, routes):
