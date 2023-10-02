ea94531f2e306859b2d7e5935b9606cad54c898a
‘ohwontaek’ <brunofied@gmail.com>
2023-10-02 18:42:49 +0900

    사각

--- 상하좌우.py
+++ 상하좌우.py
@@ -1,24 +1,21 @@
-#N을 입력받기
-n = int(input("n을 입력 하세요:  "))
+# n을 입력받기
+
+n = int(input("N을 입력"))
 
 x, y = 1,1
 
-plans = input("알파벳을 입력:  ").split()
-
+plans = input("알파벳을 입력").split()
 dx = [0,0,-1,1]
 dy = [-1,1,0,0]
-
-moves = ['L','R', 'U','D']
+moves = ['L', 'R', 'U', 'D']
 
 for plan in plans:
     for i in range(len(moves)):
         if plan == moves[i]:
             nx = x + dx[i]
             ny = y + dy[i]
-
-    if nx < 1 or ny < 1 or nx > n or ny > n:
+    if nx < 1 or ny < 1 or nx >n or ny > n:
         continue
-    #이동 수행
-
     x, y = nx, ny
+
 print(x,y)
